3fc405ab3a07be8fcdd0e3d5789a003468e81023
Alejandro <alepiumetti@hotmail.com>
2022-03-12 13:15:44 -0300

    cambio de modulo + nueva interfaz

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Import modules
 
-const prompt = require('prompt');
+var inquirer = require('inquirer');
 
 // Import schemas for inputs
 
@@ -11,67 +11,247 @@ const schemas = require('./schemas/inputs');
 const udpFunctions = require('./src/udpFunctions.js');
 const tableFunctions = require('./src/tableFunctions.js');
 
-// Constants
+// Global variables
+
+// let ip;
+// let port;
+// let ipToSend;
+// let portToSend;
 
 // Internal Functions
 
-const showTableAndWait = (interval, messageRevieved, port, ip) => {
-	return new Promise((resolve, reject) => {
-		let intervaloTabla = setInterval(async () => {
-			console.clear();
-			tableFunctions.showTable(messageRevieved, port, ip);
-			// Ask for stop
-
-			const { stop } = await prompt.get(schemas.stop);
-
-			if (stop) {
-				clearInterval(intervaloTabla);
-				resolve();
-			}
-		}, interval);
-	});
-};
-
-// Config prompt
-
-prompt.message = 'PSN - ';
-prompt.delimiter = '';
-
-// Start the prompt
-prompt.start();
-
 // Init program
-
+console.clear();
 console.log('Bienvenido a Packet Sender para NodeJs. \n');
 
-const mainFunction = async () => {
-	// Ask for IP and Port
+// Select option on menu
 
-	const { ip, port } = await prompt.get([schemas.ip, schemas.port]);
+inquirer
+	.prompt([
+		{
+			type: 'list',
+			message: 'Selecciona una opción: ',
+			name: 'option',
+			choices: ['Recibir paquetes UDP', 'Enviar paquetes UDP', 'Salir'],
+		},
+	])
+	.then((res) => {
+		switch ((key = res.option)) {
+			case 'Recibir paquetes UDP':
+				inquirer
+					.prompt([
+						{
+							type: 'input',
+							name: 'ip',
+							message:
+								'Que ip quieres escuchar?\nSi quiere escuchar todas aprete enter\nIP:',
+							validate(value) {
+								const pass = value.match(
+									/\b(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))\b/
+								);
+								if (pass) {
+									return true;
+								}
+							},
+							default() {
+								return '0.0.0.0';
+							},
+						},
+						{
+							type: 'input',
+							name: 'port',
+							message: 'Que puerto quieres escuchar?\nPuerto:',
+							validate(value) {
+								const pass = value.match(/^[0-9]+$/);
+								if (pass) {
+									return true;
+								}
+							},
+						},
+					])
+					.then(({ ip, port }) => {
+						udpFunctions.openServerUdp(ip, port);
 
-	// Open UDP server
+						let messageRevieved = []; // Array for messages - Max 30
 
-	await udpFunctions.openServerUdp(ip, port);
+						udpFunctions.listenServerUdp(messageRevieved);
 
-	let messageRevieved = []; // Array for messages - Max 30
+						inquirer
+							.prompt([
+								{
+									type: 'input',
+									name: 'interval',
+									message: 'Cuanto intervalo quieres?\nIntervalo:',
+									validate(value) {
+										const pass = value.match(/^[0-9]+$/);
+										if (pass) {
+											return true;
+										}
+									},
+									default() {
+										return 1000;
+									},
+								},
+							])
+							.then(({ interval }) => {
+								let intervaloTabla = setInterval(async () => {
+									console.clear();
+									// Ask for stop
 
-	udpFunctions.listenServerUdp(messageRevieved);
+									tableFunctions.showTable(messageRevieved, port, ip);
+									// prompt.get(schemas.stop, (err, result) => {
+									// 	if (err) {
+									// 		return console.log(err);
+									// 	}
 
-	const { interval } = await prompt.get(schemas.interval);
+									// 	stop = result.sstop;
+									// });
 
-	//Show table and wait for stop
+									// if (stop) {
+									// 	clearInterval(intervaloTabla);
+									// }
+								}, interval);
+								//Show table and wait for stop
+							})
+							.catch((error) => {
+								if (error.isTtyError) {
+									console.log(
+										'Se ha producido un error en la ejecución del programa: ',
+										error
+									);
+								} else {
+									console.log(
+										'Se ha producido un error en la ejecución del programa: ',
+										error
+									);
+								}
+							});
+					})
+					.catch((error) => {
+						if (error.isTtyError) {
+							console.log(
+								'Se ha producido un error en la ejecución del programa: ',
+								error
+							);
+						} else {
+							console.log(
+								'Se ha producido un error en la ejecución del programa: ',
+								error
+							);
+						}
+					});
+				break;
 
-	showTableAndWait(interval, messageRevieved, port, ip).then(async () => {
-		const { condicional } = await prompt.get(schemas.condicional);
+			case 'Enviar paquetes UDP':
+				console.log('Esta funcion esta en construccion');
 
-		if (condicional) {
-			const { message } = await prompt.get(schemas.message);
-			udpFunctions.sendMessageUdp(message, port, ip);
+				break;
+
+			case 'Salir':
+				console.log('Adios');
+				process.exit();
+				break;
+
+			default:
+				break;
+		}
+	})
+	.catch((error) => {
+		if (error.isTtyError) {
+			console.log(
+				'Se ha producido un error en la ejecución del programa: ',
+				error
+			);
+			process.exit();
+		} else {
+			console.log(
+				'Se ha producido un error en la ejecución del programa: ',
+				error
+			);
+			process.exit();
 		}
 	});
-};
 
-mainFunction();
+// prompt.get([schemas.menuSelect], (err, result) => {
+// 	switch (result.select) {
+// 		case 0:
+// 			// Listen UDP
+
+// 			// Ask for IP and Port
+
+// 			prompt.get([schemas.ip, schemas.port], (err, result) => {
+// 				if (err) {
+// 					return console.log(err);
+// 				}
+
+// 				ip = result.ip;
+// 				port = result.port;
+
+// 				// Open UDP server
+
+// 				udpFunctions.openServerUdp(ip, port);
+
+// 				let messageRevieved = []; // Array for messages - Max 30
+
+// 				udpFunctions.listenServerUdp(messageRevieved);
+
+// 				let interval; // Interval for show table
+// 				prompt.get(schemas.interval, (err, result) => {
+// 					if (err) {
+// 						return console.log(err);
+// 					}
+
+// 					interval = result.interval;
+
+// 					let stop = false;
+
+// 					do {
+// 						// let intervaloTabla = setInterval(async () => {
+// 						// console.clear();
+// 						// Ask for stop
+
+// 						setTimeout(() => {
+// 							tableFunctions.showTable(messageRevieved, port, ip);
+// 							prompt.get(schemas.stop, (err, result) => {
+// 								if (err) {
+// 									return console.log(err);
+// 								}
+
+// 								stop = result.stop;
+// 							});
+// 						}, interval);
+
+// 						// if (stop) {
+// 						// 	clearInterval(intervaloTabla);
+// 						// }
+// 						// }, interval);
+// 						//Show table and wait for stop
+// 					} while (!stop);
+
+// 					// const { condicional } = await prompt.get(schemas.condicional);
+
+// 					// if (condicional) {
+// 					// 	const { message } = await prompt.get(schemas.message);
+// 					// 	udpFunctions.sendMessageUdp(message, port, ip);
+// 					// } else {
+// 					// }
+// 				});
+// 			});
+
+// 			break;
+// 		case 1:
+// 			// Send UDP
+// 			break;
+
+// 		case 2:
+// 			// Exit
+// 			process.exit();
+
+// 		default:
+// 			console.log('default');
+// 			break;
+// 	}
+// });
 
 // for test
 //puerto 40060
